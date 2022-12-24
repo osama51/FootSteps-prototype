d2cd42c9e94b6d73896ef841baf155a96aae63bd
osama51 <m.osos2011@gmail.com>
2022-12-24 22:17:42 +0200

    disabled, synced, enabled databinding in gradle to detect the new packane name

--- app/src/androidTest/java/com/toddler/footsteps/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/toddler/footsteps/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.toddler.bluecomm", appContext.packageName)
+        assertEquals("com.toddler.footsteps", appContext.packageName)
     }
 }
