a2fc85783e7e74cec8d0aee0f3b0275869f318b5
Mohamed Osama <36142935+osama51@users.noreply.github.com>
2023-07-11 14:23:26 +0300

    added reqs and instructs to assess fragment, generalized their animation

--- app/src/main/java/com/toddler/footsteps/ui/AssessFragment.kt
+++ app/src/main/java/com/toddler/footsteps/ui/AssessFragment.kt
@@ -8,7 +8,9 @@ import android.view.animation.AccelerateInterpolator
 import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import com.bitvale.switcher.SwitcherC
 import com.toddler.footsteps.R
 import com.toddler.footsteps.Screens
 import com.toddler.footsteps.chat.ChatViewModel
@@ -22,9 +24,15 @@ class AssessFragment: Fragment() {
 
 
 
+    private lateinit var assessViewModel: AssessViewModel
     private lateinit var statsViewModel: StatsViewModel
     private lateinit var chatViewModel: ChatViewModel
     private lateinit var binding: FragmentAssessBinding
+
+    private lateinit var req_text_array: List<MutableLiveData<Boolean>>
+    private lateinit var req_indicator_fun_array: List<(Boolean)->Unit>
+    private lateinit var req_indicator_array: List<MutableLiveData<Boolean>>
+    private lateinit var req_switch_array: List<SwitcherC>
 
 
     override fun onCreateView(
@@ -34,6 +42,7 @@ class AssessFragment: Fragment() {
     ): View {
 
         binding = FragmentAssessBinding.inflate(inflater, container, false)
+        assessViewModel = ViewModelProvider(requireActivity())[AssessViewModel::class.java]
         statsViewModel = ViewModelProvider(requireActivity())[StatsViewModel::class.java]
         chatViewModel = ViewModelProvider(requireActivity())[ChatViewModel::class.java]
         val fragmentManager = requireActivity().supportFragmentManager
@@ -46,13 +55,132 @@ class AssessFragment: Fragment() {
         rootView.isFocusableInTouchMode = true
         rootView.setOnTouchListener { _, _ -> true }
 
+
+        // Set the lifecycle owner to this Fragment
+        // This is needed so that the binding can observe LiveData updates
+        binding.lifecycleOwner = viewLifecycleOwner // Set the lifecycle owner to this Fragment
+        binding.viewModel = assessViewModel
+
         // go back to the previous fragment upon clicking the back button on the toolbar
         binding.toolbar.setNavigationOnClickListener {
 //            requireActivity().onBackPressed()
             fragmentManager.popBackStack()
         }
 
-        return binding.root
+        req_text_array = listOf(
+            assessViewModel.req1Text,
+            assessViewModel.req2Text,
+            assessViewModel.req3Text,
+            assessViewModel.req4Text
+            )
+
+        req_switch_array = listOf(
+            binding.requirement1Switch,
+            binding.requirement2Switch,
+            binding.requirement3Switch,
+            binding.requirement4Switch
+        )
+
+        req_indicator_fun_array = listOf(
+            { value -> assessViewModel.setReq1(value) },
+            { value -> assessViewModel.setReq2(value) },
+            { value -> assessViewModel.setReq3(value) },
+            { value -> assessViewModel.setReq4(value) }
+        )
+
+        req_indicator_array = listOf(
+            assessViewModel.req1,
+            assessViewModel.req2,
+            assessViewModel.req3,
+            assessViewModel.req4
+        )
+
+        for(req_text in req_text_array) {
+            req_text.observe(viewLifecycleOwner) {
+                if(req_text.value == true) {
+                    req_switch_array[req_text_array.indexOf(req_text)].setChecked(true)
+                } else {
+                    req_switch_array[req_text_array.indexOf(req_text)].setChecked(false)
+                }
+            }
+        }
+
+        for(switch in req_switch_array) {
+            switch.setOnCheckedChangeListener { isChecked ->
+                req_indicator_fun_array[req_switch_array.indexOf(switch)](isChecked)
+//                Log.i("switch", "switch ${req_indicator_array[req_switch_array.indexOf(switch)].value} is $isChecked")
+//                req_indicator_array[req_switch_array.indexOf(switch)](isChecked)
+
+                assessViewModel.checkAllRequirements()
+            }
+        }
+
+        val timerView = binding.timerContainer
+        val requirementsContainer = binding.requirementsContainer
+        val instructionsContainer = binding.instructionsContainer
+        assessViewModel.allRequirementsMet.observe(viewLifecycleOwner) {
+            if(it == true) {
+                handleVisibility(timerView, ShowHide.SHOW, 0f, 0f, -1f, 0f)
+                handleVisibility(requirementsContainer, ShowHide.HIDE, 0f, -1f, 0f, 0f)
+                handleVisibility(instructionsContainer, ShowHide.SHOW, 1f, 0f, 0f, 0f)
+            } else {
+                handleVisibility(timerView, ShowHide.HIDE, 0f, 0f, 0f, -1f)
+                handleVisibility(requirementsContainer, ShowHide.SHOW, -1f, 0f, 0f, 0f)
+                handleVisibility(instructionsContainer, ShowHide.HIDE, 0f, 1f, 0f, 0f)
+            }
+        }
+
+        binding.instructionsBack.setOnClickListener() {
+            handleVisibility(timerView, ShowHide.HIDE, 0f, 0f, 0f, -1f)
+            handleVisibility(requirementsContainer, ShowHide.SHOW, -1f, 0f, 0f, 0f)
+            handleVisibility(instructionsContainer, ShowHide.HIDE, 0f, 1f, 0f, 0f)
+
+            for(switch in req_switch_array) {
+                switch.setChecked(false)
+            }
+        }
+
+        return rootView
+    }
+
+
+    private fun handleVisibility(view: View, showHide: ShowHide, fromX: Float, toX: Float, fromY: Float, toY: Float) {
+        var _fromY = fromY
+        var _toY = toY
+        var _fromX = fromX
+        var _toX = toX
+        var viewVisibility = View.GONE
+
+        if(showHide == ShowHide.SHOW) {
+            viewVisibility = View.VISIBLE
+            view.visibility = viewVisibility
+
+        } else if(showHide == ShowHide.HIDE) {
+            viewVisibility = View.GONE
+        }
+
+        // Create a TranslateAnimation to animate the view's position
+        val animation = TranslateAnimation(
+            Animation.RELATIVE_TO_SELF, _fromX,
+            Animation.RELATIVE_TO_SELF, _toX,
+            Animation.RELATIVE_TO_SELF, _fromY,
+            Animation.RELATIVE_TO_SELF, _toY
+        )
+
+        // Set the duration and interpolator for the animation
+        animation.duration = 500
+        animation.interpolator = AccelerateInterpolator()
+
+        // Set the AnimationListener to handle the view's visibility change
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                // Set the visibility of the view to "gone" after the animation ends
+                view.visibility = viewVisibility
+            }
+        })
+        view.startAnimation(animation)
     }
 
 
@@ -78,7 +206,6 @@ class AssessFragment: Fragment() {
 
 
     private fun showHideBottomNavBar(showHide: ShowHide) {
-
         var fromY = 0f
         var toY = 1f
         var viewVisibility = View.GONE
--- app/src/main/java/com/toddler/footsteps/ui/AssessViewModel.kt
+++ app/src/main/java/com/toddler/footsteps/ui/AssessViewModel.kt
@@ -12,10 +12,45 @@ class AssessViewModel(
     private val defaultCounterValue = 60.0
     // create a livedata variable of type double to act as counter
     private val _counter = MutableLiveData<Double>(0.0)
-
     // create a getter for the counter
     val counter: LiveData<Double>
         get() = _counter
+
+    private val _req1 = MutableLiveData<Boolean>(false)
+    val req1: MutableLiveData<Boolean>
+        get() = _req1
+
+    private val _req1Text = MutableLiveData<Boolean>(false)
+    val req1Text: MutableLiveData<Boolean>
+        get() = _req1Text
+
+    private val _req2 = MutableLiveData<Boolean>(false)
+    val req2: MutableLiveData<Boolean>
+        get() = _req2
+
+    private val _req2Text = MutableLiveData<Boolean>(false)
+    val req2Text: MutableLiveData<Boolean>
+        get() = _req2Text
+
+    private val _req3 = MutableLiveData<Boolean>(false)
+    val req3: MutableLiveData<Boolean>
+        get() = _req3
+
+    private val _req3Text = MutableLiveData<Boolean>(false)
+    val req3Text: MutableLiveData<Boolean>
+        get() = _req3Text
+
+    private val _req4 = MutableLiveData<Boolean>(false)
+    val req4: MutableLiveData<Boolean>
+        get() = _req4
+
+    private val _req5Text = MutableLiveData<Boolean>(false)
+    val req4Text: MutableLiveData<Boolean>
+        get() = _req5Text
+
+    private val _allRequirementsMet = MutableLiveData<Boolean>(false)
+    val allRequirementsMet: MutableLiveData<Boolean>
+        get() = _allRequirementsMet
 
     init {
         setCounter(defaultCounterValue)
@@ -30,5 +65,56 @@ class AssessViewModel(
             _counter.value = _counter.value?.minus(1)
 
         }
+    }
+
+    fun setReq1(req1: Boolean) {
+        this._req1.value = req1
+        this._req1Text.value = req1
+    }
+
+    fun setReq1Text() {
+//        this._req_1.value = !this._req_1.value!!
+        this._req1Text.value = !this._req1Text.value!!
+    }
+
+    fun setReq2(req2: Boolean) {
+        this._req2.value = req2
+        this._req2Text.value = req2
+    }
+
+    fun setReq2Text() {
+//        this._req_2.value = !this._req_2.value!!
+        this._req2Text.value = !this._req2Text.value!!
+    }
+
+    fun setReq3(req3: Boolean) {
+        this._req3.value = req3
+        this._req3Text.value = req3
+    }
+
+    fun setReq3Text() {
+//        this._req_3.value = !this._req_3.value!!
+        this._req3Text.value = !this._req3Text.value!!
+    }
+
+    fun setReq4(req4: Boolean) {
+        this._req4.value = req4
+        this._req5Text.value = req4
+    }
+
+    fun setReq4Text() {
+//        this._req_4.value = !this._req_4.value!!
+        this._req5Text.value = !this._req5Text.value!!
+    }
+
+    fun checkAllRequirements() {
+        val bool = _req1.value!! && _req2.value!! && _req3.value!! && _req4.value!!
+        if(_allRequirementsMet.value != bool) {
+            _allRequirementsMet.value = bool
+        }
+    }
+
+    fun resetCounter() {
+        _counter.value = defaultCounterValue
     }
 }
